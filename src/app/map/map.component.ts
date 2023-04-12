fac2f169ceeb48dd0906a7a7d4c819b85f2d5fbd
RubensGarofalo <rubensgiuseppegarofalo@gmail.com>
2023-04-12 15:53:08 +0200

    ...

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,20 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
 import Map from 'ol/Map';
 import View, { FitOptions } from 'ol/View';
-import XYZ from 'ol/source/XYZ';
 import { defaults as defaultControls } from 'ol/control';
+import { Extent } from 'ol/extent';
+import GeoJSON from 'ol/format/GeoJSON';
+import { Geometry, SimpleGeometry } from 'ol/geom';
 import { defaults as defaultInteraction } from 'ol/interaction';
 import TileLayer from 'ol/layer/Tile';
-import { Extent } from 'ol/extent';
-import { Geometry, SimpleGeometry } from 'ol/geom';
-import Vector from 'ol/source/Vector';
-import Style from 'ol/style/Style';
-import Stroke from 'ol/style/Stroke';
-import Fill from 'ol/style/Fill';
 import VectorLayer from 'ol/layer/Vector';
-import GeoJSON from 'ol/format/GeoJSON';
-import VectorSource from 'ol/source/Vector';
-import { HttpClient } from '@angular/common/http';
+import { default as Vector, default as VectorSource } from 'ol/source/Vector';
+import XYZ from 'ol/source/XYZ';
+import Fill from 'ol/style/Fill';
+import Stroke from 'ol/style/Stroke';
+import Style from 'ol/style/Style';
 
 @Component({
   selector: 'app-map',
@@ -23,65 +23,14 @@ import { HttpClient } from '@angular/common/http';
   encapsulation: ViewEncapsulation.None,
 })
 export class MapComponent implements OnInit {
-  @Input() parcel: number | undefined;
   private _view: View | undefined;
+
+  @Input() parcel: number | undefined;
+
   map: Map | undefined;
   vectorLayer: VectorLayer<Vector<Geometry>> | undefined;
 
   constructor(private _http: HttpClient) {}
-
-  ngOnInit(): void {
-    this._initMap();
-    if (this.parcel != null) {
-      this._http
-        .get<any>(this._buildParcelUrl(this.parcel))
-        .subscribe((geojson) => {
-          this._buildGeojson(geojson);
-        });
-    }
-  }
-
-  private _initMap(): void {
-    this._view = new View({
-      maxZoom: 22,
-      zoom: 5,
-      minZoom: 5,
-      projection: 'EPSG:3857',
-      constrainOnlyCenter: true,
-      showFullExtent: true,
-    });
-
-    this.map = new Map({
-      view: this._view,
-      controls: defaultControls({
-        rotate: false,
-        attribution: false,
-        zoom: false,
-      }),
-      interactions: defaultInteraction({
-        mouseWheelZoom: false,
-        dragPan: false,
-        doubleClickZoom: false,
-      }),
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://api.webmapp.it/tiles/{z}/{x}/{y}.png',
-            projection: 'EPSG:3857',
-            tileSize: [256, 256],
-            minZoom: 7,
-            maxZoom: 22,
-            cacheSize: 5000,
-          }),
-          visible: true,
-          zIndex: 0,
-          opacity: 1,
-          preload: Infinity,
-        }),
-      ],
-      target: 'ol-map',
-    });
-  }
 
   fitView(
     geometryOrExtent: SimpleGeometry | Extent,
@@ -94,6 +43,17 @@ export class MapComponent implements OnInit {
     }
     if (this._view != null) {
       this._view.fit(geometryOrExtent, optOptions);
+    }
+  }
+
+  ngOnInit(): void {
+    this._initMap();
+    if (this.parcel != null) {
+      this._http
+        .get<any>(this._buildParcelUrl(this.parcel))
+        .subscribe((geojson) => {
+          this._buildGeojson(geojson);
+        });
     }
   }
 
@@ -142,7 +102,50 @@ export class MapComponent implements OnInit {
       }
     }
   }
+
   private _buildParcelUrl(parcel: number): string {
     return `https://sisteco.maphub.it/api/v1/geom/cadastralparcel/${parcel}`;
+  }
+
+  private _initMap(): void {
+    this._view = new View({
+      maxZoom: 22,
+      zoom: 5,
+      minZoom: 5,
+      projection: 'EPSG:3857',
+      constrainOnlyCenter: true,
+      showFullExtent: true,
+    });
+
+    this.map = new Map({
+      view: this._view,
+      controls: defaultControls({
+        rotate: false,
+        attribution: false,
+        zoom: false,
+      }),
+      interactions: defaultInteraction({
+        mouseWheelZoom: false,
+        dragPan: false,
+        doubleClickZoom: false,
+      }),
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://api.webmapp.it/tiles/{z}/{x}/{y}.png',
+            projection: 'EPSG:3857',
+            tileSize: [256, 256],
+            minZoom: 7,
+            maxZoom: 22,
+            cacheSize: 5000,
+          }),
+          visible: true,
+          zIndex: 0,
+          opacity: 1,
+          preload: Infinity,
+        }),
+      ],
+      target: 'ol-map',
+    });
   }
 }
